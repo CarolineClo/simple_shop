d8d88e16d4e37903a4a7c621476411504e9c565b
caroline cloughley <caro672g@stud.kea.dk>
2022-11-13 23:52:17 +0100

    thank you message

--- src/modules/db.js
+++ src/modules/db.js
@@ -1,7 +1,7 @@
-export function insertOrder(payload) {
+export async function insertOrder(payload) {
   const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ2xuZW96amF0Y3Zham1uZmRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY2MzkwOTAsImV4cCI6MTk4MjIxNTA5MH0.5XX9jkZmpHLjQRl860FE-LldzTmlsyie-tn16yfJu7g";
   const url = "https://uiglneozjatcvajmnfdh.supabase.co";
-  fetch(url + "/rest/v1/simple_shop", {
+  const res = await fetch(url + "/rest/v1/simple_shop", {
     method: "POST",
     headers: {
       apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ2xuZW96amF0Y3Zham1uZmRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY2MzkwOTAsImV4cCI6MTk4MjIxNTA5MH0.5XX9jkZmpHLjQRl860FE-LldzTmlsyie-tn16yfJu7g",
@@ -10,28 +10,6 @@ export function insertOrder(payload) {
       Prefer: "return=representation",
     },
     body: JSON.stringify(payload),
-  })
-    .then()
-    .then();
+  });
+  return await res.json();
 }
-
-/*
-
-const options = {
-  method: 'POST',
-  headers: {
-    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ2xuZW96amF0Y3Zham1uZmRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY2MzkwOTAsImV4cCI6MTk4MjIxNTA5MH0.5XX9jkZmpHLjQRl860FE-LldzTmlsyie-tn16yfJu7g',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ2xuZW96amF0Y3Zham1uZmRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY2MzkwOTAsImV4cCI6MTk4MjIxNTA5MH0.5XX9jkZmpHLjQRl860FE-LldzTmlsyie-tn16yfJu7g',
-    'Content-Type': 'application/json',
-    Prefer: 'return=representation'
-  },
-  body: '{"won":true,"name":"Settlers","rating":4}'
-};
-
-fetch('https://uiglneozjatcvajmnfdh.supabase.co/rest/v1/boardgames', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
-
-*/
